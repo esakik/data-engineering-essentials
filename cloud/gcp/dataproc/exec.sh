aa900a63c8195c8805971e0d20e4883d62413c06
esaki01 <social0430@gmail.com>
2020-02-16 01:49:16 +0900

    Refactoring

--- cloud/gcp/dataproc/exec.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python src/most_used_word.py -r dataproc sample.txt -c config.yaml --output-dir=gs://dataproc-01/output/
---
+++ cloud/gcp/dataproc/on-dataproc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python src/most_used_word.py -r dataproc -c config.yaml input.txt --output-dir=gs://dataproc-01/output/
