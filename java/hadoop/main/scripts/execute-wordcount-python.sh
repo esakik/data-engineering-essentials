877ac4f39f0956911eec963df86908b1d04990e1
esakik <social0430@gmail.com>
2020-04-02 22:49:51 +0900

    Change directory

--- java/hadoop/main/scripts/execute-wordcount-python.sh
+++ java/hadoop/main/scripts/execute-wordcount-python.sh
@@ -3,7 +3,7 @@
 # Execute word count application
 sudo -u hdfs hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.6.0-mr1-cdh5.16.2.jar \
 -input /user/hdfs/input/input.txt -output /user/hdfs/output02 \
--mapper /streaming/python/map.py -reducer /streaming/python/reduce.py
+-mapper /main/streaming/python/map.py -reducer /main/streaming/python/reduce.py
 
 # Show result
 sudo -u hdfs hadoop fs -ls /user/hdfs/output02
